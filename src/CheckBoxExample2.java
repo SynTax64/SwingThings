dd97d7204456de5c5123d40158b9bdfbb6d1a80d
David Aster <davidaster007@gmail.com>
2021-04-12 09:44:16 +0200

    added Food Ordering System with Checkbox components

---
+++ src/CheckBoxExample2.java
@@ -0,0 +1,87 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+public class CheckBoxExample2 extends JFrame implements ActionListener {
+	JLabel labelTitle;
+	JLabel labelPizza;
+	JLabel labelBurger;
+	JLabel labelTea;
+
+	JCheckBox checkboxPizza;
+	JCheckBox checkboxBurger;
+	JCheckBox checkboxTea;
+
+	JButton buttonOrder;
+
+	public CheckBoxExample2() {
+		labelTitle = new JLabel("Food Ordering System");
+		labelPizza = new JLabel("Pizza 8.7 EUR");
+		labelBurger = new JLabel("Burger 4.5 EUR");
+		labelTea = new JLabel("Tea 2.5 EUR");
+
+		checkboxPizza = new JCheckBox();
+		checkboxBurger = new JCheckBox();
+		checkboxTea = new JCheckBox();
+
+		buttonOrder = new JButton("Order");
+
+		checkboxPizza.setBounds(60, 80, 20, 20);
+		checkboxBurger.setBounds(60, 110, 20, 20);
+		checkboxTea.setBounds(60, 140, 20, 20);
+
+		labelTitle.setBounds(120, 40, 200, 20);
+		labelPizza.setBounds(80, 80, 120, 20);
+		labelBurger.setBounds(80, 110, 120, 20);
+		labelTea.setBounds(80, 140, 120, 20);
+		buttonOrder.setBounds(90, 180, 150, 30);
+
+		add(labelTitle);
+		add(labelPizza);
+		add(labelBurger);
+		add(labelTea);
+		add(checkboxPizza);
+		add(checkboxBurger);
+		add(checkboxTea);
+		add(buttonOrder);
+		buttonOrder.addActionListener(this);
+		setSize(350, 300);
+		setLayout(null);
+		setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		double amount = 0;
+		StringBuilder outputOrder = new StringBuilder();
+
+		if (checkboxPizza.isSelected()) {
+			amount += 8.7;
+			outputOrder.append("Pizza 8.7 EUR\n");
+		}
+
+		if (checkboxBurger.isSelected()) {
+			amount += 4.5;
+			outputOrder.append("Burger 4.5 EUR\n");
+		}
+
+		if (checkboxTea.isSelected()) {
+			amount += 2.5;
+			outputOrder.append("Tea 2.5 EUR\n");
+		}
+
+		outputOrder.append("===========\n");
+		outputOrder.append("Total: " + amount + " EUR");
+
+		JOptionPane.showMessageDialog(this, outputOrder);
+	}
+
+	public static void main(String[] args) {
+		CheckBoxExample2 JObjekt = new CheckBoxExample2();
+	}
+}
--- src/JCheckBoxExample.java
+++ src/JCheckBoxExample.java
@@ -1,8 +1,42 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class JCheckBoxExample extends JFrame {
 
 	public JCheckBoxExample() {
+		JCheckBox cb1 = new JCheckBox("C++");
+		JCheckBox cb2 = new JCheckBox("Java");
+		JLabel l = new JLabel();
+
+		cb1.setBounds(70, 50, 100, 50);
+		cb2.setBounds(70, 100, 100, 50);
+		l.setBounds(70, 20, 200, 20);
+
+		add(cb1);
+		add(cb2);
+		add(l);
+
+		cb1.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				l.setText("C++ Checkbox: " + (e.getStateChange() == 1 ? "checked" : "unchecked"));
+			}
+		});
+
+		cb2.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				l.setText("Java Checkbox: " + (e.getStateChange() == 1 ? "checked" : "unchecked"));
+			}
+		});
 
 		setLayout(null);
 		setSize(400, 600);
