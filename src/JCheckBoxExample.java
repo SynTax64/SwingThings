e8885d05f8e75fc71c7984303a2cb081fdb4217c
David A. <davidaster007@gmail.com>
2021-04-12 07:50:08 +0200

    added JCheckbox example

---
+++ src/JCheckBoxExample.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class JCheckBoxExample extends JFrame {
+
+	public JCheckBoxExample() {
+
+		setLayout(null);
+		setSize(400, 600);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		JCheckBoxExample checkBoxExample = new JCheckBoxExample();
+	}
+}
